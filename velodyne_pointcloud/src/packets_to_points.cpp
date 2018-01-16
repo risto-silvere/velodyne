fb5be881b1e57cf29ce598950ce4332d10bdd384
Gustaf Lönn <gustaf.lonn@silvere.com>
2018-01-16 09:21:56 +0200

    IMU timestamps corrected.

--- velodyne_pointcloud/src/packets_to_points.cpp
+++ velodyne_pointcloud/src/packets_to_points.cpp
@@ -3,6 +3,7 @@
 #include <rosbag/bag.h>
 #include <velodyne_msgs/VelodynePacket.h>
 #include <rosbag/view.h>
+#include <sensor_msgs/Imu.h>
 #include <boost/foreach.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 #include <boost/filesystem.hpp>
@@ -14,31 +15,32 @@ void add_bag(rosbag::Bag *bagin, rosbag::Bag *bagout, velodyne_rawdata::RawData*
     rosbag::View view(*bagin);
     BOOST_FOREACH(rosbag::MessageInstance const m, view) {
         if (m.getTopic() == "/velodyne_packets") {
-        velodyne_msgs::VelodyneScan::ConstPtr scanMsg = m.instantiate<velodyne_msgs::VelodyneScan>();
-        if (scanMsg != NULL) {
-            for (size_t i = 0; i < scanMsg->packets.size(); ++i) {
-                velodyne_rawdata::VPointCloud::Ptr outMsg(new velodyne_rawdata::VPointCloud());
-                outMsg->header.frame_id = "horizontal_vlp16_link";
-                outMsg->height = 1;
-                data_->unpack(scanMsg->packets[i], *outMsg);
-                uint32_t sec_in_hour_ros = scanMsg->header.stamp.sec % 3600;
-                uint32_t sec_in_hour_gps = outMsg->header.stamp / 1000000;
-                uint32_t sec;
-                if (sec_in_hour_ros > 3000 && sec_in_hour_gps < 600)
-                    sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps + 3600;
-                else if (sec_in_hour_gps > 3000 && sec_in_hour_ros < 600)
-                    sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps - 3600;
-                else
-                    sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps;
-                uint32_t nsec = (outMsg->header.stamp % 1000000) * 1000;
-                outMsg->header.stamp = pcl_conversions::toPCL(ros::Time(sec, nsec));
-                (*bagout).write("/horizontal_laser_3d", ros::Time(sec, nsec), outMsg);
+            velodyne_msgs::VelodyneScan::ConstPtr scanMsg = m.instantiate<velodyne_msgs::VelodyneScan>();
+            if (scanMsg != NULL) {
+                for (size_t i = 0; i < scanMsg->packets.size(); ++i) {
+                    velodyne_rawdata::VPointCloud::Ptr outMsg(new velodyne_rawdata::VPointCloud());
+                    outMsg->header.frame_id = "horizontal_vlp16_link";
+                    outMsg->height = 1;
+                    data_->unpack(scanMsg->packets[i], *outMsg);
+                    uint32_t sec_in_hour_ros = scanMsg->header.stamp.sec % 3600;
+                    uint32_t sec_in_hour_gps = outMsg->header.stamp / 1000000;
+                    uint32_t sec;
+                    if (sec_in_hour_ros > 3000 && sec_in_hour_gps < 600)
+                        sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps + 3600;
+                    else if (sec_in_hour_gps > 3000 && sec_in_hour_ros < 600)
+                        sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps - 3600;
+                    else
+                        sec = scanMsg->header.stamp.sec - sec_in_hour_ros + sec_in_hour_gps;
+                    uint32_t nsec = (outMsg->header.stamp % 1000000) * 1000;
+                    outMsg->header.stamp = pcl_conversions::toPCL(ros::Time(sec, nsec));
+                    (*bagout).write("/horizontal_laser_3d", ros::Time(sec, nsec), outMsg);
+                }
             }
-        } } else if (m.getTopic() == "/vectornav/imu")  {
-(*bagout).write("/imu", m.getTime(), m, m.getConnectionHeader());
-  }
-else {
-          (*bagout).write(m.getTopic(), m.getTime(), m, m.getConnectionHeader());
+        } else if (m.getTopic() == "/vectornav/imu") {
+            sensor_msgs::Imu::ConstPtr imu_data = m.instantiate<sensor_msgs::Imu>();
+            (*bagout).write("/imu", imu_data->header.stamp, m, m.getConnectionHeader());
+        } else {
+            (*bagout).write(m.getTopic(), m.getTime(), m, m.getConnectionHeader());
         }
     }
 }
